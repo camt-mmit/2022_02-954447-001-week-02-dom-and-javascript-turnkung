6f7cc869b7028748fe5435cdbd506f987c907165
turnkung <40079500+turnkung@users.noreply.github.com>
2022-12-26 14:59:56 +0700

    Delete src directory

--- src/js/dom-06-final.js
+++
@@ -1,8 +0,0 @@
-import {assign as assignInput} from './input.js';
-
-document.addEventListener('DOMContentLoaded', () => {
-    const inputSection = document.querySelector('.cmp-input-section');
-    const inputTemplate = document.querySelector('template#tmp-input');
-
-    assignInput(inputSection, inputTemplate);
-});
